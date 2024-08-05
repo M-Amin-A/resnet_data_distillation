9ef3d7ce4a37ad178cd937b1bd5f7409a8d4730d
M.Amin <m.a.abbasfar@gmail.com>
2024-08-05 12:05:32 +0330

    bug

--- train_jempp.py
+++ train_jempp.py
@@ -73,7 +73,8 @@ def sample_q(f, replay_buffer, y=None, n_steps=10, in_steps=10, args=None, save=
     init_sample, buffer_inds = sample_p_0(replay_buffer, bs=bs, y=y)
     x_k = t.autograd.Variable(init_sample, requires_grad=True)
     # sgld
-    print("here", in_steps, f.f.layer_one)
+    print(f.f)
+    print(f.f.layer_one)
     if in_steps > 0:
         Hamiltonian_func = Hamiltonian(f.f.layer_one)
 
